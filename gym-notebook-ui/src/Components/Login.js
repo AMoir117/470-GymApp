def2ea1e11bedab1bb43e7e7e9b2a35c48dd8f14
christopher murray <chrisware707@gmail.com>
2022-10-25 18:09:36 -0700

    added react-native-paper, still working on login-page

--- gym-notebook-ui/src/Components/Login.js
+++ gym-notebook-ui/src/Components/Login.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from "react";
-import {SafeAreaView, Text, StyleSheet, View, FlatList, TextInput, Button} from "react-native";
+import {SafeAreaView, Text, StyleSheet, View, FlatList, TextInput, Pressable} from "react-native";
+import {Divider} from "react-native-paper";
 import axios from "axios";
 
 const styles = StyleSheet.create({
@@ -12,48 +13,83 @@ const styles = StyleSheet.create({
 		width: 250,
 		borderWidth: 1,
 		paddingLeft: 10,
-		margin: 5,
+		marginTop: 30,
 		alignSelf: "center",
-		borderColor: "#009688",
+		borderColor: "#000000",
 		backgroundColor: "#FFFFFF",
 	},
 	textForgetPassword: {
-		fontSize: 5,
+		fontSize: 10,
+		color: "#0073ff",
 		alignSelf: "center",
 	},
 	buttonContainer: {
-		height: 40,
+		height: 30,
 		width: 130,
-		borderWidth: 1,
-		margin: 5,
 		flexDirection: "row",
-		alignContent: "center",
 		alignSelf: "center",
 	},
 	buttonStyle: {
-		height: 40,
-		width: 50,
-		borderWidth: 1,
-		borderColor: "#FFFFFF",
-		backgroundColor: "#FFFFFF",
+		height: 30,
+		width: 70,
+		margin: 2,
+		marginTop: 10,
+		borderColor: "#949494",
+		backgroundColor: "#949494",
+	},
+	buttonText: {
+		fontSize: 20,
+		color: "#000000",
+		textAlign: "center",
+		textAlignVertical: "center",
 	},
 });
 const Login = () => {
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
+
 	useEffect(() => {}, []);
 
 	const login = () => {};
 
+	const getUsername = (text) => {
+		setUsername(text);
+	};
+	const getPassword = (text) => {
+		setPassword(text);
+	};
+
 	return (
 		<SafeAreaView style={{flex: 1}}>
 			<Text style={styles.textTitle}>GYM NOTEBOOK</Text>
-			<TextInput style={styles.textInputStyle} value={"Username"} />
-			<TextInput style={styles.textInputStyle} value={"Password"} />
+			<Divider
+				style={{borderColor: "#ff0000", borderWidth: 3, borderRadius: 5}}
+				horizontalInset="3"
+			/>
+			<TextInput
+				style={styles.textInputStyle}
+				placeholder={"Username"}
+				value={username}
+				onChange={(text) => getUsername(text)}
+			/>
+			<TextInput
+				style={styles.textInputStyle}
+				placeholder={"Password"}
+				value={password}
+				onChange={(text) => getPassword(text)}
+			/>
 			<View style={styles.textForgetPassword}>
-				<Button color={"#026df7"} onPress={login} title="FORGET PASSWORD?" />
+				<Pressable color={"#026df7"} onPress={login}>
+					<Text style={styles.textForgetPassword}>FORGOT PASSWORD?</Text>
+				</Pressable>
 			</View>
 			<View style={styles.buttonContainer}>
-				<Button color={"#000000"} onPress={login} title="Login" />
-				<Button color={"#000000"} onPress={login} title="Signup" />
+				<Pressable style={styles.buttonStyle} onPress={login}>
+					<Text style={styles.buttonText}>Login</Text>
+				</Pressable>
+				<Pressable style={styles.buttonStyle} onPress={login}>
+					<Text style={styles.buttonText}>Signup</Text>
+				</Pressable>
 			</View>
 		</SafeAreaView>
 	);
