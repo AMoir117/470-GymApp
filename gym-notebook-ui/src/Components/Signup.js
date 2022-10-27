4a61b3892ad357abe3e5dbe306663df45f9e80d3
christopher murray <chrisware707@gmail.com>
2022-10-26 21:03:01 -0700

    added scroll view and profile pic proto to signup page

--- gym-notebook-ui/src/Components/Signup.js
+++ gym-notebook-ui/src/Components/Signup.js
@@ -1,9 +1,21 @@
 import React, {useState, useEffect} from "react";
-import {SafeAreaView, Text, StyleSheet, View, FlatList, TextInput, Pressable} from "react-native";
-import {Divider} from "react-native-paper";
+import {
+	ScrollView,
+	Text,
+	StyleSheet,
+	View,
+	FlatList,
+	TextInput,
+	Pressable,
+	SafeAreaView,
+} from "react-native";
+import {Divider, Appbar, Button, Avatar} from "react-native-paper";
 import axios from "axios";
 
 const styles = StyleSheet.create({
+	areaView: {
+		height: 1200,
+	},
 	textTitle: {
 		fontSize: 30,
 		alignSelf: "center",
@@ -29,15 +41,15 @@ const styles = StyleSheet.create({
 		backgroundColor: "#FFFFFF",
 		textAlignVertical: "top",
 	},
-	textForgetPassword: {
-		fontSize: 10,
+	textUploadImage: {
+		fontSize: 15,
 		color: "#0073ff",
 		alignSelf: "center",
 	},
-	buttonContainer: {
-		height: 30,
-		width: 130,
-		flexDirection: "row",
+	buttonSave: {
+		height: 40,
+		width: 100,
+		marginTop: 20,
 		alignSelf: "center",
 	},
 	buttonStyle: {
@@ -53,62 +65,113 @@ const styles = StyleSheet.create({
 		color: "#000000",
 		textAlign: "center",
 	},
+	avatarStyle: {
+		alignSelf: "center",
+		margin: 15,
+	},
 });
 const Signup = (props) => {
 	const [username, setUsername] = useState("");
 	const [password, setPassword] = useState("");
 	const [firstName, setFirstName] = useState("");
 	const [lastName, setLastName] = useState("");
+	const [email, setEmail] = useState("");
 	const [bio, setBio] = useState("");
 
 	useEffect(() => {}, []);
 
 	return (
-		<SafeAreaView style={{flex: 1}}>
-			<Text style={styles.textTitle}>GYM NOTEBOOK</Text>
-			<Divider
-				style={{borderColor: "#ff0000", borderWidth: 3, borderRadius: 5}}
-				horizontalInset="3"
-			/>
-			<TextInput
-				style={styles.textInputStyle}
-				placeholder={"Username"}
-				value={username}
-				onChangeText={setUsername}
-			/>
-			<TextInput
-				style={styles.textInputStyle}
-				placeholder={"First Name"}
-				value={firstName}
-				onChangeText={setFirstName}
-			/>
-			<TextInput
-				style={styles.textInputStyle}
-				placeholder={"Last Name"}
-				value={lastName}
-				onChangeText={setLastName}
-			/>
-			<TextInput
-				style={styles.bioInputStyle}
-				placeholder={"Bio"}
-				value={bio}
-				multiline={true}
-				onChangeText={setBio}
-			/>
-			<TextInput
-				style={styles.textInputStyle}
-				secureTextEntry={true}
-				placeholder={"Password"}
-				value={password}
-				onChangeText={setPassword}
-			/>
-			<TextInput
-				style={styles.textInputStyle}
-				secureTextEntry={true}
-				placeholder={"Re-input Password"}
-				value={password}
-				onChangeText={setPassword}
-			/>
+		<SafeAreaView style={{flex: 1, maxHeight: "100%"}}>
+			<ScrollView
+				style={{flex: 1, maxHeight: "100%"}}
+				stickyHeaderIndices={[0]}
+				showsVerticalScrollIndicator={false}
+				alwaysBounceVertical={false}
+			>
+				<View>
+					<Appbar.Header alignSelf="center">
+						<Appbar.BackAction
+							onPress={() => {
+								/*todo::upload Image to avatar */
+							}}
+						/>
+						<Appbar.Content title="MY PROFILE" mode="center-align" alignSelf="center" />
+					</Appbar.Header>
+
+					<Divider
+						style={{borderColor: "#ff0000", borderWidth: 3, borderRadius: 5}}
+						horizontalInset="3"
+					/>
+				</View>
+				<Avatar.Image
+					style={styles.avatarStyle}
+					size={150}
+					source={require("../../assets/pexels-anush-gorak-1431283.jpg")}
+				/>
+				<View>
+					<Pressable color={"#026df7"} onPress={() => {}}>
+						<Text style={styles.textUploadImage}>Upload Image</Text>
+					</Pressable>
+				</View>
+				<TextInput
+					style={styles.textInputStyle}
+					placeholder={"Username"}
+					value={username}
+					textContentType={"username"}
+					onChangeText={setUsername}
+				/>
+				<TextInput
+					style={styles.textInputStyle}
+					placeholder={"First Name"}
+					value={firstName}
+					textContentType={"givenName"}
+					onChangeText={setFirstName}
+				/>
+				<TextInput
+					style={styles.textInputStyle}
+					placeholder={"Last Name"}
+					value={lastName}
+					textContentType={"lastName"}
+					onChangeText={setLastName}
+				/>
+				<TextInput
+					style={styles.textInputStyle}
+					placeholder={"Email"}
+					value={email}
+					textContentType={"emailAddress"}
+					onChangeText={setEmail}
+				/>
+				<TextInput
+					style={styles.bioInputStyle}
+					placeholder={"Bio"}
+					value={bio}
+					multiline={true}
+					onChangeText={setBio}
+				/>
+				<TextInput
+					style={styles.textInputStyle}
+					secureTextEntry={true}
+					placeholder={"Password"}
+					value={password}
+					textContentType={"emailAddress"}
+					onChangeText={setPassword}
+				/>
+				<TextInput
+					style={styles.textInputStyle}
+					secureTextEntry={true}
+					placeholder={"Re-input Password"}
+					value={password}
+					textContentType={"emailAddress"}
+					onChangeText={setPassword}
+				/>
+				<Button
+					style={styles.buttonSave}
+					icon="content-save"
+					mode="contained"
+					buttonColor="red"
+					onPress={() => console.log("profile saved")}
+				></Button>
+			</ScrollView>
 		</SafeAreaView>
 	);
 };
