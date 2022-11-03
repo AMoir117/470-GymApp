48c07c3c1cc8bf05d8e2c8be7a01fb352c580a25
christopher murray <chrisware707@gmail.com>
2022-11-03 00:02:38 -0700

    added extra files to gitignore

--- gym-notebook-api/app/Controllers/ExerciseController.js
+++ gym-notebook-api/app/Controllers/ExerciseController.js
@@ -1,40 +1,42 @@
-const dbConnection = require('../../database/mySQLconnect');
-const dateFormat = require('dateformat');
+const dbConnection = require("../../database/mySQLconnect");
+const dateFormat = require("dateformat");
 
 function now() {
-    return dateFormat(new Date(), "yyyy-mm-dd HH:MM:ss");
+	return dateFormat(new Date(), "yyyy-mm-dd HH:MM:ss");
 }
 
 const allExercises = async (ctx) => {
-    console.log('exercises all exercises called.');
-    return new Promise((resolve, reject) => {
-        const query = `
+	console.log("exercises all exercises called.");
+	return new Promise((resolve, reject) => {
+		const query = `
                        SELECT *
                         FROM 
-                            exercises
+                            exercise
                         ORDER BY id
                         `;
-        dbConnection.query({
-            sql: query,
-        }, (error, tuples) => {
-            if (error) {
-                console.log("Connection error in ExercisesController::allExercises", error);
-                return reject(error);
-            }
-            ctx.body = tuples;
-            ctx.status = 200;
-            return resolve();
-        });
-    }).catch(err => {
-        console.log("Database connection error in allExercises.", err);
-        // The UI side will have to look for the value of status and
-        // if it is not 200, act appropriately.
-        ctx.body = [];
-        ctx.status = 500;
-    });
-}
-
+		dbConnection.query(
+			{
+				sql: query,
+			},
+			(error, tuples) => {
+				if (error) {
+					console.log("Connection error in ExercisesController::allExercises", error);
+					return reject(error);
+				}
+				ctx.body = tuples;
+				ctx.status = 200;
+				return resolve();
+			}
+		);
+	}).catch((err) => {
+		console.log("Database connection error in allExercises.", err);
+		// The UI side will have to look for the value of status and
+		// if it is not 200, act appropriately.
+		ctx.body = [];
+		ctx.status = 500;
+	});
+};
 
 module.exports = {
-    allExercises
+	allExercises,
 };
--- gym-notebook-api/config/exercisesRoutes.js
+++ gym-notebook-api/config/exercisesRoutes.js
@@ -1,6 +1,5 @@
-const Authorize = require('../app/Middleware/Authorize.js');
-const VerifyJWT = require('../app/Middleware/VerifyJWT.js');
-
+const Authorize = require("../app/Middleware/Authorize.js");
+const VerifyJWT = require("../app/Middleware/VerifyJWT.js");
 
 /*
 |--------------------------------------------------------------------------
@@ -11,13 +10,13 @@ const VerifyJWT = require('../app/Middleware/VerifyJWT.js');
 | controller. Also used as a parent container for the other routers.
 |
 */
-const router = require('koa-router')({
-    prefix: '/api/v1'
+const router = require("koa-router")({
+	prefix: "/api/v1",
 });
 
-router.get('/', function (ctx) {
-    console.log('router.get(/)');
-    return ctx.body = 'What is up?';
+router.get("/", function (ctx) {
+	console.log("router.get(/)");
+	return (ctx.body = "What is up??");
 });
 
 /*
@@ -31,34 +30,33 @@ router.get('/', function (ctx) {
 
 // Login router configuration.
 
-const LoginController = require('../app/Controllers/LoginController.js');
-const loginRouter = require('koa-router')({
-    prefix: '/login'
+const LoginController = require("../app/Controllers/LoginController.js");
+const loginRouter = require("koa-router")({
+	prefix: "/login",
 });
-loginRouter.get('/:user_id', LoginController.authorizeUser, (err) => console.log("exercisesRoutes.js: login-route error:", err));
+loginRouter.get("/:user_id", LoginController.authorizeUser, (err) =>
+	console.log("exercisesRoutes.js: login-route error:", err)
+);
 
 // Routes router configuration.
 
-const ExercisesController = require('../app/Controllers/ExerciseController.js');
-const exercisesRouter = require('koa-router')({
-    prefix: '/exercises'
+const ExercisesController = require("../app/Controllers/ExerciseController.js");
+const exercisesRouter = require("koa-router")({
+	prefix: "/exercises",
 });
 
 //exercisesRouter.use(VerifyJWT);
-exercisesRouter.get('/all-exercises', ExercisesController.allExercises, err => console.log(`allExercises ran into an error: ${err}`));
+exercisesRouter.get("/all-exercises", ExercisesController.allExercises, (err) =>
+	console.log(`allExercises ran into an error: ${err}`)
+);
 //exercisesRouter.get('/:routeID/', Authorize('admin'), ExercisesController.exerciseWithExerciseID);
-
 
 /**
  * Register all of the controllers into the default controller.
  */
-router.use(
-    '',
-    loginRouter.routes(),
-    exercisesRouter.routes()
-);
+router.use("", loginRouter.routes(), exercisesRouter.routes());
 
 module.exports = function (app) {
-    app.use(router.routes());
-    app.use(router.allowedMethods());
+	app.use(router.routes());
+	app.use(router.allowedMethods());
 };
