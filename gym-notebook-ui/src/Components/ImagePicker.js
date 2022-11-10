f418f7dc32ffcfae7522bd8bfb22a78093ee146e
christopher murray <chrisware707@gmail.com>
2022-11-10 13:56:10 -0800

    Change root component in node-modules/expo/AppEntry.js to Index

--- gym-notebook-ui/src/Components/ImagePicker.js
+++ gym-notebook-ui/src/Components/ImagePicker.js
@@ -24,13 +24,14 @@ const ImagePick = (props) => {
 
 	const pickImage = async () => {
 		let result = await ImagePicker.launchImageLibraryAsync({
+			//base64: true,
 			mediaTypes: ImagePicker.MediaTypeOptions.All,
 			allowsEditing: true,
 			aspect: [4, 3],
 			quality: 1,
 		});
 
-		console.log(result.uri);
+		console.log(result);
 
 		if (!result.cancelled) {
 			setImage(result.uri);
