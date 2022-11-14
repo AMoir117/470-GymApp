b05c00b9d0955db157f6b0af760e1b05d9875665
christopher murray <chrisware707@gmail.com>
2022-11-13 19:18:23 -0800

    rebased

--- gym-notebook-api/app/Controllers/UsersController.js
+++ gym-notebook-api/app/Controllers/UsersController.js
@@ -98,35 +98,6 @@ const userByID = (ctx) => {
 		ctx.status = 500;
 	});
 };
-	console.log("users user by id called.");
-	return new Promise((resolve, reject) => {
-		const query = `SELECT * FROM Users WHERE id = ?;`;
-		dbConnection.query(
-			{
-				sql: query,
-				values: [ctx.params.id],
-			},
-			(error, tuples) => {
-				if (error) {
-					console.log("Connection error in UsersController::userByID", error);
-					ctx.body = [];
-					ctx.status = 200;
-					return reject(error);
-				}
-				ctx.body = tuples;
-				ctx.status = 200;
-				return resolve();
-			}
-		);
-		console.log(ctx.params);
-	}).catch((err) => {
-		console.log("Database connection error in userByID.", err);
-		// The UI side will have to look for the value of status and
-		// if it is not 200, act appropriately.
-		ctx.body = [];
-		ctx.status = 500;
-	});
-};
 
 const insertNewUser = (ctx) => {
 	const usersTableAttributes = [
@@ -237,127 +208,21 @@ const getUsersFollowers = async (ctx) => {
 };
 
 const useWeeklySchedule = async (ctx) => {
-    console.log('useWeeklySchedule called.');
-    return new Promise((resolve, reject) => {
-        const query = `
+	console.log("useWeeklySchedule called.");
+	return new Promise((resolve, reject) => {
+		const query = `
                       UPDATE Users
                       SET currentWeeklyScheduleID = ?
                       WHERE id = ?;
                       `;
-        dbConnection.query({
-            sql: query,
-            values: [ctx.params.currentWeeklyScheduleID, ctx.params.userID]
-        }, (error, tuples) => {
-            if (error) {
-                console.log("Connection error in userController::useWeeklySchedule", error);
-                return reject(error);
-            }
-            ctx.body = tuples;
-            ctx.status = 200;
-            return resolve();
-        });
-    }).catch(err => {
-        console.log("Database connection error in useWeeklySchedule.", err);
-        // The UI side will have to look for the value of status and
-        // if it is not 200, act appropriately.
-        ctx.body = [];
-        ctx.status = 500;
-    });
-}
-
-
-const insertNewUser = (ctx) => {
-	const usersTableAttributes = [
-		"username",
-		"userPassword",
-		"firstName",
-		"lastName",
-		"DoB",
-		"imagePath",
-		"email",
-		"profileBio",
-	];
-
-	let valuesFromRequest = JSON.parse(JSON.stringify(ctx.request.body)); // make a deep copy of ctx.request.body
-
-	// Pad the values that we received through the request with the default values.
-	const valuesToInsert = {
-		...valuesFromRequest,
-		...{
-			// default values
-			username: valuesFromRequest["username"],
-			userPassword: valuesFromRequest["userPassword"],
-			firstName: valuesFromRequest["firstName"],
-			lastName: valuesFromRequest["lastName"],
-			DoB: valuesFromRequest["DoB"],
-			imagePath: valuesFromRequest["imagePath"],
-			email: valuesFromRequest["email"],
-			profileBio: valuesFromRequest["profileBio"],
-		},
-	};
-
-	const valueMarkers = Array(usersTableAttributes.length).fill("?").join(", ");
-
-	// insert into tableName (a list of attributes) value(a list of values);
-
-	// valueMarkers now contains eight (routesTableAttributes.length) question marks like so: '?, ?, ?, ?, ?, ?, ?, ?';
-	const attributeValuesArray = usersTableAttributes.reduce((valuesAssembled, attribute) => {
-		valuesAssembled.push(valuesToInsert[attribute]);
-		return valuesAssembled;
-	}, []);
-
-	// attributeValuesArray also has routesTableAttributes.length element. Its element i contains
-	// values[ routeTableAttributes[i] ]. For example, attributeValuesArray[5] contains 'Active'
-
-	return new Promise((resolve, reject) => {
-		console.log(`API server::insertNewUser: ${JSON.stringify(ctx.request.body)}`);
-		console.log(
-			`API server::insertNewUser after having added default values: ${JSON.stringify(
-				valuesToInsert
-			)}`
-		);
-
-		const query = `
-                       INSERT INTO users (${usersTableAttributes}) 
-                              VALUES (${valueMarkers})
-                        `;
 		dbConnection.query(
 			{
 				sql: query,
-				values: attributeValuesArray,
+				values: [ctx.params.currentWeeklyScheduleID, ctx.params.userID],
 			},
 			(error, tuples) => {
 				if (error) {
-					console.log("Connection error in UsersController::userWithUserID", error);
-					ctx.body = [];
-					ctx.status = 200;
-					return reject(error);
-				}
-				ctx.body = tuples;
-				ctx.status = 200;
-				return resolve();
-			}
-		);
-	}).catch((error) => console.log(`insertNewUser failed with error message, ${error}`));
-};
-
-const getUsersFollowers = async (ctx) => {
-	console.log("getUsersFollowers called.");
-	return new Promise((resolve, reject) => {
-		const query = `
-                      SELECT * FROM Users Users_Table
-                      LEFT JOIN Follower Follower_Table
-                      ON Follower_Table.followerUserID = Users_Table.id
-                      WHERE Follower_Table.followedUserID = ?;
-                      `;
-		dbConnection.query(
-			{
-				sql: query,
-				values: [ctx.params.followedUserID],
-			},
-			(error, tuples) => {
-				if (error) {
-					console.log("Connection error in UsersController::getUsersFollowers", error);
+					console.log("Connection error in userController::useWeeklySchedule", error);
 					return reject(error);
 				}
 				ctx.body = tuples;
@@ -366,7 +231,7 @@ const getUsersFollowers = async (ctx) => {
 			}
 		);
 	}).catch((err) => {
-		console.log("Database connection error in getUsersFollowers.", err);
+		console.log("Database connection error in useWeeklySchedule.", err);
 		// The UI side will have to look for the value of status and
 		// if it is not 200, act appropriately.
 		ctx.body = [];
@@ -380,5 +245,5 @@ module.exports = {
 	userByID,
 	insertNewUser,
 	getUsersFollowers,
-  useWeeklySchedule
+	useWeeklySchedule,
 };
